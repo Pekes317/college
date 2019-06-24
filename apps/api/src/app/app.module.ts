2c05ac5bff81017f040e1ca2f14a0b5307436d58
Pekes317 <el_dante565@yahoo.com>
2019-06-23 23:16:14 -0500

    troubleshooting SSR

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { loadPackage } from '@nestjs/common/utils/load-package.util';
+import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const path = join(process.cwd(), 'dist');
+
 @Module({
-  imports: [],
+  imports: [
+    AngularUniversalModule.forRoot({
+      bundle: loadPackage(join(path, 'server', 'main.js'), 'SSR'),
+      viewsPath: join(path, 'views')
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
--- apps/college/src/app/app.server.module.ts
+++ apps/college/src/app/app.server.module.ts
@@ -1,14 +1,29 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import {
+  ServerModule,
+  ServerTransferStateModule
+} from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { UniversalInterceptorService } from './shared/interceptors/universal-interceptor.service';
 
 @NgModule({
   imports: [
     AppModule,
+    ModuleMapLoaderModule,
     ServerModule,
+    ServerTransferStateModule
   ],
-  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UniversalInterceptorService,
+      multi: true // <-- important (you can have many interceptors)
+    }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppServerModule {}
